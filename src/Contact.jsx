47ea72fa2c23b33472d8a856fbb0f38388bc69d2
Ilham <ilhamsr05@gmail.com>
2024-11-26 10:37:09 +0700

    new commit best1

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,13 +4,15 @@ import instagramIcon from '/assets/instagram.png';
 import gmailIcon from '/assets/ggmail.png';
 import linkedinIcon from '/assets/linkedin.png';
 import outlookIcon from '/assets/Outlook.png';
+// import whatsappicon from '/assets/whatsapp.png';
+import notionicon from '/assets/notion.png';
 
 const Contact = () => {
   return (
-    <section className="relative inset-0 flex justify-center items-center min-h-screen mt-[-125vh]" id="contact">
+    <section className="relative inset-0 flex justify-center items-center min-h-screen sm:mt-[-130vh]" id="contact">
       <div className="text-center">
         <h1 className="font-semibold sm:text-4xl text-2xl font-generalisans text-neutral-400 font-bold text-xl text-white transition-colors">
-          Contact Me:
+          Contact And OtherLinks:
         </h1>
         <div className="flex justify-center items-center mt-10 space-x-4">
           <a href="https://www.linkedin.com/in/ilhamsyahrulramadhan/" target="_blank" rel="noopener noreferrer">
@@ -24,6 +26,9 @@ const Contact = () => {
           </a>
           <a href="https://www.instagram.com/_ilhamsr" target="_blank" rel="noopener noreferrer">
             <Button name="Instagram" containerClass="w-full" icon={instagramIcon} />
+          </a>
+          <a href="https://lean-ceder-c38.notion.site/Hi-I-m-Ilham-Syahrul-Ramadhan-1066ca1720cd80b6ab61c47c82b7fd90" target="_blank" rel="noopener noreferrer">
+            <Button name="Notion (CV)" containerClass="w-full" icon={notionicon} />
           </a>
           
         </div>
